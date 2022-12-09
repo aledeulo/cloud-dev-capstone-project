9f49eb17b648cf7fd6da2ef3e4c6e6a87450504b
aledeulo <alew3bslution@gmail.com>
2022-12-09 10:58:59 +0700

    fix auth

--- client/src/config.ts
+++ client/src/config.ts
@@ -5,5 +5,5 @@ export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com
 export const authConfig = {
   domain: process.env.REACT_APP_AUTH_DOMAIN,            // Auth0 domain
   clientId: process.env.REACT_APP_CLIENT_ID,          // Auth0 client id
-  callbackUrl: 'http://a0dc9360bb2a949e4b6dc6c4be089223-1009354281.us-east-1.elb.amazonaws.com/callback'
+  callbackUrl: 'http://a0dc9360bb2a949e4b6dc6c4be089223-1009354281.us-east-1.elb.amazonaws.com//callback'
 }
